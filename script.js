807655e56c7632fa02b73778f6e98eb2708a9718
viniciusmacedo12b <vm8399469@gmail.com>
2024-05-09 10:03:01 -0300

    adicionado começo de transição

---
+++ script.js
@@ -0,0 +1,41 @@
+
+function transitionindex()
+{
+const transition = document.querySelector('.transition');
+transition.setAttribute('class', 'animation');
+const button = document.querySelector('.card');
+button.setAttribute('class', 'conteudoanimation');
+const p = document.querySelector('.paragrafo');
+p.setAttribute('class', 'conteudoanimation');
+const h1 = document.querySelector('.top');
+h1.setAttribute('class', 'conteudoanimation');
+setTimeout(function() {
+    window.location.replace("http://127.0.0.1:5500/animaisT.html");
+  }, 1500);
+}
+function transitionanimaisT()
+{
+    const transitiona = document.querySelector('.transition');
+    transitiona.setAttribute('class', 'animation');
+    const buttona = document.querySelector('.card');
+    buttona.setAttribute('class', 'conteudoanimation');
+    const pa = document.querySelector('.paragrafo');
+    pa.setAttribute('class', 'conteudoanimation');
+    const h1a = document.querySelector('.top');
+    h1a.setAttribute('class', 'conteudoanimation');
+    setTimeout(function() {
+        window.location.replace("http://127.0.0.1:5500/taticas.html") }, 1500);
+}
+function transitionanimaisTvoltar()
+{
+    const transitionv = document.querySelector('.transition');
+    transitionv.setAttribute('class', 'animation');
+    const buttonv = document.querySelector('.card');
+    buttonv.setAttribute('class', 'conteudoanimation');
+    const pv = document.querySelector('.paragrafo');
+    pv.setAttribute('class', 'conteudoanimation');
+    const h1v = document.querySelector('.top');
+    h1v.setAttribute('class', 'conteudoanimation');
+    setTimeout(function() {
+        window.location.replace("http://127.0.0.1:5500/index.html") }, 1500);
+}
